c38a398e5c1cca9aa230eb50bff702138f87ba34
alexb60 <alexandrebertrand60@yahoo.fr>
2023-01-16 09:48:45 +0100

    Statuts Chambres début

---
+++ GestionHotel/GestionHotel/Data/Profiles/StatutsChambresProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using GestionHotel.Data.Dtos;
+using GestionHotel.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionHotel.Data.Profiles
+{
+    public class StatutsChambresProfiles : Profile
+    {
+        public StatutsChambresProfiles()
+        {
+            CreateMap<StatutsChambre, StatutsChambreDTO>();
+            CreateMap<StatutsChambreDTO, StatutsChambre>();
+        }
+    }
+}
